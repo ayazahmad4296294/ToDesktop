895d309adc7212a075ed64898338fdd17ace64dc
ayazahmad4296294 <ayazahmad4296294@gmail.com>
2025-12-23 18:31:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function handleMenu() {
+    const dialog = document.getElementById("nav-dialog");
+    dialog.classList.toggle("hidden");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            colors: { 'primary': '#3238f2', },
+            fontFamily: {
+                'display': ['Poppins', 'sans-serif'],
+                'body': ['Inter', 'sans-serif']
+            },
+        },
+    },
+    plugins: [],
+}
