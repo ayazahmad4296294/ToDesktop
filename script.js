7ab5e0988fb5c335b04ce533d8b80a3753fc4c78
ayazahmad4296294 <ayazahmad4296294@gmail.com>
2025-12-24 11:12:55 +0500

    js carousel

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function animate() {
         if (!el) return;
 
         const current =
-            parseFloat(el.dataset.x || 0) + delta * 0.5 * dir;
+            parseFloat(el.dataset.x || 0) + delta * 0.3 * dir;
 
         el.dataset.x = current;
         el.style.transform = `translateX(${current}px)`;
